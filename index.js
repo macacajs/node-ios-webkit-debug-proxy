431f6bb64a1ad34d5e13fd1a7585f9106c3b086d
xudafeng <xudafeng@126.com>
2016-03-02 14:27:35 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/* ================================================================
+ * node-ios-webkit-debug-proxy by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Mar 02 2016 14:20:50 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+module.exports = require('./lib/node-ios-webkit-debug-proxy');
---
+++ test/node-ios-webkit-debug-proxy.test.js
@@ -0,0 +1,22 @@
+/* ================================================================
+ * node-ios-webkit-debug-proxy by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Mar 02 2016 14:20:50 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var Proxy = require('..');
+
+describe('test', function() {
+  it('should be ok', function() {
+    
+  });
+});
